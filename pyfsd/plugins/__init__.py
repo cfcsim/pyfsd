0b515027e3231b12cf62afcbe219c45647bd1b60
gamecss <52902973+gamecss@users.noreply.github.com>
2023-06-18 13:27:51 +0800

    Fix typing error

--- pyfsd/plugins/__init__.py
+++ pyfsd/plugins/__init__.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from twisted.plugin import pluginPackagePaths
 
 __path__.extend(pluginPackagePaths(__name__))  # type: ignore
-del pluginPackagePaths
-__all__ = []
+__all__: List[str] = []
+del pluginPackagePaths, List
