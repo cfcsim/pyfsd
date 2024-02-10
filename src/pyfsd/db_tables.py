fe28ae19923b6c33bf5885644704297413eacfb8
gamecss <52902973+gamecss@users.noreply.github.com>
2024-02-10 11:41:42 +0800

    refactor: change database password length to argon2, remove auth.py

--- src/pyfsd/db_tables.py
+++ src/pyfsd/db_tables.py
@@ -1,10 +1,17 @@
+"""PyFSD database tables.
+
+Note:
+    These databases were initialized in pyfsd.main.main().
+"""
 from sqlalchemy import Column, Integer, MetaData, String, Table
 
+__all__ = ["metadata", "users_table"]
+
 metadata = MetaData()
-users = Table(
+users_table = Table(
     "users",
     metadata,
     Column("callsign", String, primary_key=True),
-    Column("password", String(64)),
+    Column("password", String(32)),
     Column("rating", Integer()),
 )
