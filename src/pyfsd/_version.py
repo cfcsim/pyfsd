751e3dddf44d7066164e8111e3c5dcaf73957db0
gamecss <52902973+gamecss@users.noreply.github.com>
2024-02-10 22:55:03 +0800

    fix(._version,.utils): restore _version.py, add docstring

--- src/pyfsd/_version.py
+++ src/pyfsd/_version.py
@@ -1,1 +1,17 @@
-version = "0.0.1.2.dev16+g63ffda2.d20231223"
+# Will replaced by pdm while building wheel
+__all__ = ["version"]
+DEFAULT_VERSION = "0.0.1.2.dev0"
+version: str
+
+try:
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version as version_lookup
+
+    try:
+        version = version_lookup("pyfsd")
+    except PackageNotFoundError:
+        version = DEFAULT_VERSION
+
+    del PackageNotFoundError, version_lookup
+except ImportError:
+    version = DEFAULT_VERSION
---
+++ src/pyfsd/utils/__init__.py
@@ -0,0 +1,1 @@
+"""PyFSD cli utilies."""
